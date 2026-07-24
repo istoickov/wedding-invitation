d152115d1397e22e448f361195aacd77474a7c5d
Ivan Stoickov <iv.stoickov@gmail.com>
2026-07-25 00:54:42 +0200

    Make first screen photo bigger;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 export const Header = () => (
   <header className="absolute top-0 z-50 flex w-full items-center justify-center py-6 sm:py-8">
-    <div className="font-headline text-2xl md:text-3xl text-primary tracking-widest">Б & И</div>
+    <div className="site-monogram font-headline text-2xl md:text-3xl text-primary tracking-widest">Б & И</div>
   </header>
 );
